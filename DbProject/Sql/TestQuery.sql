d44b0efab2e4a37072097cc62eff04258e265c57
KirillMelnikovDev <kirillmelnikov.dev@gmail.com>
2023-11-06 22:54:16 +0300

    Added sql

---
+++ DbProject/Sql/TestQuery.sql
@@ -0,0 +1,7 @@
+﻿-- using created database "Mindbox"
+use Mindbox;
+
+select Product.Name, Category.Name
+from dbo.Products Product
+left join dbo.ProductsCategories ProductsToCategories on Product.Id = ProductsToCategories.ProductId
+left join dbo.Categories Category on ProductsToCategories.CategoryId = Category.Id;
